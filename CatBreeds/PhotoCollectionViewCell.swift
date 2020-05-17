77fb85455949fd33de28e67029a8410250669e0a
Olga <olga.zinch.mail@gmail.com>
2020-05-17 21:44:54 +0300

    gallery screen and image viewer

---
+++ CatBreeds/PhotoCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CatBreeds
+//
+//  Created by Olga on 17.05.2020.
+//  Copyright © 2020 Olga. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
