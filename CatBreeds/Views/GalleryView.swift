a15a9752dd68a5a01f25b3c4d64d757cd30df61e
olgazinchenko <ozinchenko.pm@gmail.com>
2023-01-04 08:16:01 +0100

    Refactoring: GalleryView added

---
+++ CatBreeds/Views/GalleryView.swift
@@ -0,0 +1,13 @@
+//
+//  GalleryView.swift
+//  CatBreeds
+//
+//  Created by ozinchenko.dev on 04/01/2023.
+//  Copyright © 2023 Olga. All rights reserved.
+//
+
+import UIKit
+
+class GalleryView: UIView {
+    
+}
